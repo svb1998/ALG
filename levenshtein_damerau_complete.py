018e83240445ca2857f53e82d86b9c7bcd2e0892
CarlosCobas <carlos.cobas1997@gmail.com>
2020-10-25 14:13:36 +0100

    Mi version de los ejercicios, contrastando con lo de Steven todo funciona igual. En esta version esta completado el ejercicio de la distancia de leveshtein damerau intermedia

---
+++ levenshtein_damerau_complete.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+def dp_levenshtein_backwards(x, y):
+    D = np.zeros((len(x) + 1, len(y) + 1))
+    for i in range(1, len(x) + 1):
+        D[i, 0] = D[i - 1, 0] + 1
+    for j in range(1, len(y) + 1):
+        D[0, j] = D[0, j - 1] + 1
+        for i in range(1, len(x) + 1):
+            D[i, j] = min(
+                D[i - 1, j] + 1,
+                D[i, j - 1] + 1,
+                D[i - 1, j - 1] + (x[i-1] != y[j-1]) 
+            )
+    return D[len(x), len(y)]
+
+def dp_restricted_damerau_backwards(x, y):
+    D = np.zeros((len(x) + 1, len(y) + 1))
+    for i in range(1, len(x) + 1):
+        D[i, 0] = D[i - 1, 0] + 1
+    for j in range(1, len(y) + 1):
+        D[0, j] = D[0, j - 1] + 1
+        for i in range(1, len(x) + 1):
+            D[i, j] = min(
+                D[i - 1, j] + 1,
+                D[i, j - 1] + 1,
+                D[i - 1, j - 1] + (x[i-1] != y[j-1]) 
+            )
+            if i > 1 and j > 1 and x[i - 2] == y[j - 1] and x[i - 1] == y[j-2]:
+                D[i, j] = min(
+                    D[i, j],
+                    D[i - 2, j - 2] + 1
+                )
+    return D[len(x), len(y)]
+
+def dp_intermediate_damerau_backwards(x, y):
+    D = np.zeros((len(x) + 1, len(y) + 1))
+    for i in range(1, len(x) + 1):
+        D[i, 0] = D[i - 1, 0] + 1
+    for j in range(1, len(y) + 1):
+        D[0, j] = D[0, j - 1] + 1
+        for i in range(1, len(x) + 1):
+            D[i, j] = min(
+                D[i - 1, j] + 1,
+                D[i, j - 1] + 1,
+                D[i - 1, j - 1] + (x[i-1] != y[j-1]) 
+            )
+            if i > 1 and j > 1 and x[i - 2] == y[j - 1] and x[i - 1] == y[j-2]:
+                D[i, j] = min(
+                    D[i, j],
+                    D[i - 2, j - 2] + 1
+                )
+            if(i > 2 and j > 1 and x[i - 3] == y[j - 1] and x[i - 1] == y[j - 2]):
+                D[i, j] = min(
+                    D[i, j],
+                    D[i - 2, j - 2] + 1
+                )
+            if(i > 1 and j > 2 and x[i - 1] == y[j - 3] and x[i - 2] == y[j - 1]):
+                D[i, j] = min(
+                    D[i, j],
+                    D[i - 2, j - 2] + 1
+                )
+    return D[len(x), len(y)]
+
+test = [("algoritmo","algortimo"),
+        ("algoritmo","algortximo"),
+        ("algoritmo","lagortimo"),
+        ("algoritmo","agaloritom"),
+        ("algoritmo","algormio"),
+        ("acb","ba")]
+
+for x,y in test:
+    print(f"{x:12} {y:12}",end="")
+    for dist,name in ((dp_levenshtein_backwards,"levenshtein"),
+                      (dp_restricted_damerau_backwards,"restricted"),
+                      (dp_intermediate_damerau_backwards,"intermediate")):
+        print(f" {name} {dist(x,y):2}",end="")
+    print()
+                 
+"""
+Salida del programa:
+
+algoritmo    algortimo    levenshtein  2 restricted  1 intermediate  1
+algoritmo    algortximo   levenshtein  3 restricted  3 intermediate  2
+algoritmo    lagortimo    levenshtein  4 restricted  2 intermediate  2
+algoritmo    agaloritom   levenshtein  5 restricted  4 intermediate  3
+algoritmo    algormio     levenshtein  3 restricted  3 intermediate  2
+acb          ba           levenshtein  3 restricted  3 intermediate  2
+"""         
