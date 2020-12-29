92f8e64983fa45cc126caded4ac1a68df52f6116
Steven Valencia <steven.valencia1998@outlook.com>
2020-12-29 16:42:20 +0100

    Suggest sin trie con las 3 distancias

--- compare_algorithms.py
+++ compare_algorithms.py
@@ -1,4 +1,4 @@
-from spellsuggest import TrieSpellSuggester
+from spellsuggest import TrieSpellSuggester, SpellSuggester
 import time
 import collections
 import re
@@ -11,8 +11,32 @@ import re
 dicts = ["quijote_0_100.txt", "quijote_0_500.txt", "quijote_0_1000.txt"]
 
 for dict in dicts:
-    spellsuggester = TrieSpellSuggester("./corpora/"+dict)
+
+    spellsuggester = SpellSuggester("./corpora/"+dict)
+    trie_spellsuggester = TrieSpellSuggester("./corpora/"+dict)
+
+    #Levenshtein 
     t_ini = time.process_time()
-    res = spellsuggester.suggest("progremador", "levenshtein", 1)
+    res = spellsuggester.suggest("casa", "levenshtein")
     t_end = time.process_time()
-    print("Tiempo usando "+dict+":\t"+str(t_end-t_ini))
+    print("[Distancia Levenshtein] -> Tiempo usando "+dict+":\t"+str(t_end-t_ini))
+
+    #Restringida
+    t_ini = time.process_time()
+    res = spellsuggester.suggest("casa", "restricted")
+    t_end = time.process_time()
+    print("[Distancia Levenshtein Restringida] -> Tiempo usando "+dict+":\t"+str(t_end-t_ini))
+
+    #Intermedia
+    t_ini = time.process_time()
+    res = spellsuggester.suggest("casa", "intermediate")
+    t_end = time.process_time()
+    print("[Distancia Levenshtein Intermedia] -> Tiempo usando "+dict+":\t"+str(t_end-t_ini))
+    
+    #Con Trie
+    t_ini = time.process_time()
+    res = trie_spellsuggester.suggest("progremador", "levenshtein", 1)
+    t_end = time.process_time()
+    print("[Distancia Levenshtein Con Trie] -> Tiempo usando "+dict+":\t"+str(t_end-t_ini))
+
+    print("----------------------------------------------------------")
